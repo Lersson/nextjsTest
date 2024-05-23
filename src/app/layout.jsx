32af5bb894df6d5b0ce0cf79aba0c312cd30042f
lersson.lisboa <lersson.lisboa@meioambiente.mg.gov.br>
2024-05-23 14:59:38 -0300

    volta fontes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-// const inter = Inter({ subsets: ["latin"],display: "swap" });
+const inter = Inter({ subsets: ["latin"],display: "swap" });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,8 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <body className={inter.className}>{children}</body> */}
-      <body className={''}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
